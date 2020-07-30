fcfc8b1330c3a480ce6a6108ef2a8cc04f393ae9
Leo <shiyi.du@qq.com>
2020-07-30 14:02:02 -0400

    Gif Created

--- src/Shiyi/Main.java
+++ src/Shiyi/Main.java
@@ -9,87 +9,87 @@ import java.util.concurrent.TimeUnit;
 
 public class Main {
 
-    public static void initSnakeGame2() throws InterruptedException {
+	public static void initSnakeGame2() throws InterruptedException {
 
-        SnakeGame2 gameModel = new SnakeGame2(20, 20);
-        SnakeLearner2 learner = new SnakeLearner2(gameModel);
+		SnakeGame2 gameModel = new SnakeGame2(20, 20);
+		SnakeLearner2 learner = new SnakeLearner2(gameModel);
 
 //        learner.stopLearning();
 
-        SnakeListener gameListener = new SnakeListener(gameModel.getBody());
+		SnakeListener gameListener = new SnakeListener(gameModel.getBody());
 
-        initGL(gameListener);
+		initGL(gameListener);
 
-        while (true) {
-            learner.learn();
-            gameListener.setFinderPosition((int) gameModel.getFinderPosition().getX(), (int) gameModel.getFinderPosition().getY());
-            gameListener.setApplePosition((int) gameModel.getApplePosition().getX(), (int) gameModel.getApplePosition().getY());
-            if (learner.currentGeneration() >= 230000) TimeUnit.MILLISECONDS.sleep(100);
+		while (true) {
+			learner.learn();
+			gameListener.setFinderPosition((int) gameModel.getFinderPosition().getX(), (int) gameModel.getFinderPosition().getY());
+			gameListener.setApplePosition((int) gameModel.getApplePosition().getX(), (int) gameModel.getApplePosition().getY());
+			if (learner.currentGeneration() >= 230000) TimeUnit.MILLISECONDS.sleep(33);
 //            else TimeUnit.MILLISECONDS.sleep(10);
-        }
-    }
+		}
+	}
 
-    public static void initSnakeGame() throws InterruptedException {
+	public static void initSnakeGame() throws InterruptedException {
 
-        SnakeGame gameModel = new SnakeGame(20, 20);
-        SnakeLearner learner = new SnakeLearner(gameModel);
+		SnakeGame gameModel = new SnakeGame(20, 20);
+		SnakeLearner learner = new SnakeLearner(gameModel);
 
-        SnakeListener gameListener = new SnakeListener(gameModel.getBody());
+		SnakeListener gameListener = new SnakeListener(gameModel.getBody());
 
-        initGL(gameListener);
+		initGL(gameListener);
 
-        while (true) {
-            learner.learn();
-            gameListener.setFinderPosition((int) gameModel.getFinderPosition().getX(), (int) gameModel.getFinderPosition().getY());
-            gameListener.setApplePosition((int) gameModel.getApplePosition().getX(), (int) gameModel.getApplePosition().getY());
-            if (learner.currentGeneration() >= 0) TimeUnit.MILLISECONDS.sleep(100);
+		while (true) {
+			learner.learn();
+			gameListener.setFinderPosition((int) gameModel.getFinderPosition().getX(), (int) gameModel.getFinderPosition().getY());
+			gameListener.setApplePosition((int) gameModel.getApplePosition().getX(), (int) gameModel.getApplePosition().getY());
+			if (learner.currentGeneration() >= 0) TimeUnit.MILLISECONDS.sleep(100);
 //            else TimeUnit.MILLISECONDS.sleep(10);
-        }
-    }
+		}
+	}
 
-    public static void initAppleFinderGame() throws InterruptedException {
+	public static void initAppleFinderGame() throws InterruptedException {
 
-        AppleFinderGame gameModel = new AppleFinderGame(20, 20);
-        AppleFinderLearner learner = new AppleFinderLearner(gameModel);
+		AppleFinderGame gameModel = new AppleFinderGame(20, 20);
+		AppleFinderLearner learner = new AppleFinderLearner(gameModel);
 
-        AppleFinderListener gameListener = new AppleFinderListener();
+		AppleFinderListener gameListener = new AppleFinderListener();
 
-        initGL(gameListener);
+		initGL(gameListener);
 
-        while (true) {
-            learner.learn();
-            gameListener.setFinderPosition((int) gameModel.getFinderPosition().getX(), (int) gameModel.getFinderPosition().getY());
-            gameListener.setApplePosition((int) gameModel.getApplePosition().getX(), (int) gameModel.getApplePosition().getY());
-            TimeUnit.MILLISECONDS.sleep(100);
-        }
-    }
+		while (true) {
+			learner.learn();
+			gameListener.setFinderPosition((int) gameModel.getFinderPosition().getX(), (int) gameModel.getFinderPosition().getY());
+			gameListener.setApplePosition((int) gameModel.getApplePosition().getX(), (int) gameModel.getApplePosition().getY());
+			TimeUnit.MILLISECONDS.sleep(100);
+		}
+	}
 
-    private static void initGL(AppleFinderListener listener) {
-        GLProfile.initSingleton();
-        GLProfile profile = GLProfile.get(GLProfile.GL2);
-        GLCapabilities caps = new GLCapabilities(profile);
+	private static void initGL(AppleFinderListener listener) {
+		GLProfile.initSingleton();
+		GLProfile profile = GLProfile.get(GLProfile.GL2);
+		GLCapabilities caps = new GLCapabilities(profile);
 
-        GLWindow window = GLWindow.create(caps);
-        window.setSize(800, 800);
-        window.setResizable(false);
-        window.addGLEventListener(listener);
-        window.addKeyListener(new KeyInput());
+		GLWindow window = GLWindow.create(caps);
+		window.setSize(800, 800);
+		window.setResizable(false);
+		window.addGLEventListener(listener);
+		window.addKeyListener(new KeyInput());
 
-        FPSAnimator animator = new FPSAnimator(window, 60);
-        animator.start();
+		FPSAnimator animator = new FPSAnimator(window, 60);
+		animator.start();
 
-        window.setVisible(true);
-    }
+		window.setVisible(true);
+	}
 
-    public static void main(String[] args) {
-        try {
+	public static void main(String[] args) {
+		try {
 //            initAppleFinderGame();
 //            initSnakeGame();
-            initSnakeGame2();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+			initSnakeGame2();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 
 
-    }
+	}
 }
--- src/Shiyi/SnakeLearner2.java
+++ src/Shiyi/SnakeLearner2.java
@@ -12,162 +12,162 @@ import java.util.List;
 import java.util.stream.Stream;
 
 public class SnakeLearner2 {
-    //this class is used to connect apple finder game and Q_learner
-    String myName = "Snake_learner104857";
-    Q_Learner learner;
-    SnakeGame2 gameModel;
-    private float lastDistance;
-    private int lastScore;
-    private float reward;
-    //    private float greedyDecay = 0.0002f; //how less greedy should we get after each move?
-    private int generation = 0;
-    private int highestScore = 0;
-    private int sumScore = 0;
-    private boolean stopedLearning = false;
+	//this class is used to connect apple finder game and Q_learner
+	String myName = "Snake_learner104857";
+	Q_Learner learner;
+	SnakeGame2 gameModel;
+	private float lastDistance;
+	private int lastScore;
+	private float reward;
+	//    private float greedyDecay = 0.0002f; //how less greedy should we get after each move?
+	private int generation;
+	private int highestScore = 0;
+	private int sumScore = 0;
+	private boolean stopedLearning = false;
 
-    public SnakeLearner2(SnakeGame2 gameModel) {
-        this.gameModel = gameModel;
-        this.learner = new Q_Learner(0.2f, 0.8f, 0.5f, 1048576, 4, this::getReward, this::executeAction);
-//        loadData("Snake_learner104857_training_data_gen230000");
-        generation = 0;
-    }
+	public SnakeLearner2(SnakeGame2 gameModel) {
+		this.gameModel = gameModel;
+		this.learner = new Q_Learner(0.2f, 0.8f, 0.5f, 1048576, 4, this::getReward, this::executeAction);
+		loadData("Snake_learner104857_training_data_gen230000");
+		generation = 230000;
+	}
 
-    public void stopLearning() {
-        stopedLearning = true;
-        learner.stopLearning();
-    }
+	public void stopLearning() {
+		stopedLearning = true;
+		learner.stopLearning();
+	}
 
-    public int currentGeneration() {
-        return generation;
-    }
+	public int currentGeneration() {
+		return generation;
+	}
 
-    public void learn() {
-        this.learner.MoveAndLearn();
+	public void learn() {
+		this.learner.MoveAndLearn();
 //        this.learner.decreaseGreedy(greedyDecay);
 //        System.out.println("Distance:" + lastDistance + " Reward" + reward);
 //        System.out.println("State:" + gameModel.getState() + "Score: " + gameModel.getScore());
 //        System.out.println("current generation:" + generation);
-    }
+	}
 
-    public void loadData(String fileName) {
-        StringBuilder contentBuilder = new StringBuilder();
-        List<List<Float>> qTable = new ArrayList<>();
+	public void loadData(String fileName) {
+		StringBuilder contentBuilder = new StringBuilder();
+		List<List<Float>> qTable = new ArrayList<>();
 
-        String filePath = "training_data/" + fileName;
-        System.out.println("loading training data from: " + filePath);
-        try (Stream<String> stream = Files.lines(Paths.get(filePath), StandardCharsets.UTF_8)) {
-            stream.forEach(s -> contentBuilder.append(s).append("\n"));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+		String filePath = "training_data/" + fileName;
+		System.out.println("loading training data from: " + filePath);
+		try (Stream<String> stream = Files.lines(Paths.get(filePath), StandardCharsets.UTF_8)) {
+			stream.forEach(s -> contentBuilder.append(s).append("\n"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
-        for (String state : contentBuilder.toString().split(";")) {
-            if (state.isBlank()) continue;
-            List<Float> row = new ArrayList<>();
-            for (String value : state.split(",")) {
-                if (value.isBlank()) continue;
-                row.add(Float.parseFloat(value));
-            }
-            if (row.size() != 4) System.out.println("ERROR: ACTION LENGTH NOT CORRECT");
-            qTable.add(row);
-        }
+		for (String state : contentBuilder.toString().split(";")) {
+			if (state.isBlank()) continue;
+			List<Float> row = new ArrayList<>();
+			for (String value : state.split(",")) {
+				if (value.isBlank()) continue;
+				row.add(Float.parseFloat(value));
+			}
+			if (row.size() != 4) System.out.println("ERROR: ACTION LENGTH NOT CORRECT");
+			qTable.add(row);
+		}
 
-        learner.setqTable(qTable);
-        System.out.println("finished loading data." + qTable.size());
-    }
+		learner.setqTable(qTable);
+		System.out.println("finished loading data." + qTable.size());
+	}
 
-    public void saveData() {
-        int counter = 0;
-        String name = this.myName + "new_training_data_gen" + ((Integer) generation).toString();
-        File newFile = new File("training_data/" + name);
-        while (newFile.exists()) {
-            counter++;
-            name = name.substring(0, name.length() - 1) + ((Integer) counter).toString();
-            newFile = new File("training_data/" + name);
-        }
+	public void saveData() {
+		int counter = 0;
+		String name = this.myName + "new_training_data_gen" + ((Integer) generation).toString();
+		File newFile = new File("training_data/" + name);
+		while (newFile.exists()) {
+			counter++;
+			name = name.substring(0, name.length() - 1) + ((Integer) counter).toString();
+			newFile = new File("training_data/" + name);
+		}
 
-        StringBuilder result = new StringBuilder();
+		StringBuilder result = new StringBuilder();
 
-        List<List<Float>> qTable = learner.getqTable();
-        for (List<Float> actionValue : qTable) {
-            for (Float f : actionValue) {
-                result.append(",");
-                result.append(f.toString());
-            }
-            result.append(";\n");
-        }
+		List<List<Float>> qTable = learner.getqTable();
+		for (List<Float> actionValue : qTable) {
+			for (Float f : actionValue) {
+				result.append(",");
+				result.append(f.toString());
+			}
+			result.append(";\n");
+		}
 
-        try {
-            FileWriter writer = new FileWriter(newFile);
-            writer.write(result.toString());
-            writer.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+		try {
+			FileWriter writer = new FileWriter(newFile);
+			writer.write(result.toString());
+			writer.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
-        System.out.println("data saved");
-    }
+		System.out.println("data saved");
+	}
 
-    private int executeAction(int action) {
-        //0 left, 1 up, 2 right, 3 down
-        lastDistance = gameModel.distanceToApple();
-        lastScore = gameModel.getScore();
-        switch (action) {
-            case 0:
-                gameModel.moveLeft();
-                break;
-            case 1:
-                gameModel.moveUp();
-                break;
-            case 2:
-                gameModel.moveRight();
-                break;
-            case 3:
-                gameModel.moveDown();
-                break;
-        }
-        int currentScore = gameModel.getScore();
-        float currentDistance = gameModel.distanceToApple();
+	private int executeAction(int action) {
+		//0 left, 1 up, 2 right, 3 down
+		lastDistance = gameModel.distanceToApple();
+		lastScore = gameModel.getScore();
+		switch (action) {
+			case 0:
+				gameModel.moveLeft();
+				break;
+			case 1:
+				gameModel.moveUp();
+				break;
+			case 2:
+				gameModel.moveRight();
+				break;
+			case 3:
+				gameModel.moveDown();
+				break;
+		}
+		int currentScore = gameModel.getScore();
+		float currentDistance = gameModel.distanceToApple();
 
-        if (currentScore > lastScore) {
-            reward = 3;
-            lastScore = currentScore;
-            if (stopedLearning) System.out.println("current score:" + currentScore);
-        } else if (currentScore < lastScore) {
-            reward = -100;
-            generation++;
-            sumScore += lastScore;
-            highestScore = Math.max(lastScore, highestScore);
-            float newGreedy = (float) (1f / (float) (highestScore * 200f));
-            if (!stopedLearning) learner.setGreedyFactor(newGreedy);
-            if (generation % 100 == 0) {
-                System.out.println("current generation:" + generation);
-                System.out.println("new greedy factor:" + newGreedy);
-                System.out.println("average score:" + sumScore / 100f);
-                sumScore = 0;
-                System.out.println("high score:" + highestScore);
-            }
-            if (generation % 10000 == 0) saveData();
-            //train it for certain amount of generations
+		if (currentScore > lastScore) {
+			reward = 3;
+			lastScore = currentScore;
+			if (stopedLearning) System.out.println("current score:" + currentScore);
+		} else if (currentScore < lastScore) {
+			reward = -100;
+			generation++;
+			sumScore += lastScore;
+			highestScore = Math.max(lastScore, highestScore);
+			float newGreedy = (float) (1f / (float) (highestScore * 200f));
+			if (!stopedLearning) learner.setGreedyFactor(newGreedy);
+			if (generation % 100 == 0) {
+				System.out.println("current generation:" + generation);
+				System.out.println("new greedy factor:" + newGreedy);
+				System.out.println("average score:" + sumScore / 100f);
+				sumScore = 0;
+				System.out.println("high score:" + highestScore);
+			}
+			if (generation % 10000 == 0) saveData();
+			//train it for certain amount of generations
 //            if (generation > training) {
 //                learner.decreaseGreedy(1f);
 //            }
-            lastScore = currentScore;
-        } else if (currentDistance < lastDistance) {
-            reward = 2;
-            lastDistance = currentDistance;
-        } else if (currentDistance == lastDistance) {
-            reward = 1;
-            lastDistance = currentDistance;
-        } else if (currentDistance > lastDistance) {
-            reward = 1;
-            lastDistance = currentDistance;
-        }
+			lastScore = currentScore;
+		} else if (currentDistance < lastDistance) {
+			reward = 2;
+			lastDistance = currentDistance;
+		} else if (currentDistance == lastDistance) {
+			reward = 1;
+			lastDistance = currentDistance;
+		} else if (currentDistance > lastDistance) {
+			reward = 1;
+			lastDistance = currentDistance;
+		}
 
-        return gameModel.getState();
-    }
+		return gameModel.getState();
+	}
 
-    private float getReward() {
-        return reward;
-    }
+	private float getReward() {
+		return reward;
+	}
 }
